02c8d2de6fe00e8abdbec2dc50506df8a53ab3df
Yomaira Escano <yjescano@gmail.com>
2018-03-19 04:02:57 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -23,7 +23,7 @@ class RecipesController < ApplicationController
     params.require(:recipe).permit(
     :title, 
     ingredients_attributes: [
-      :id
+      :id, 
       :name,
       :quantity
       ]
